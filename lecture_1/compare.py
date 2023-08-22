f0a139c8662e2a5348257a8e421528f08da6aa8c
Noah <ninadurkes@gmail.com>
2023-08-22 19:33:19 +0200

    completed lecture 1

---
+++ lecture_1/compare.py
@@ -0,0 +1,11 @@
+x = int(input("x? "))
+y = int(input("y? "))
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("y is less than x")
+else:
+    print("they are the same")
+
+# or allows you to decide between more alternatives
---
+++ lecture_1/grade.py
@@ -0,0 +1,14 @@
+score = int(input("Score: "))
+
+if score >= 100:
+    print("A")
+elif score >= 90:
+    print("B")
+elif score >= 80:
+    print("C")
+elif score >= 70:
+    print("D")
+elif score >= 60:
+    print("E")
+else:
+    print("F")
