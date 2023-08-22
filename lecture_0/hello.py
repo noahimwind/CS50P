3fc83c94193ad8820559e0164603ddba591cc85c
Noah <ninadurkes@gmail.com>
2023-08-22 13:21:19 +0200

    first commit

---
+++ lecture_0/hello.py
@@ -0,0 +1,9 @@
+# FUNCTIONS AND VARIABLES
+# use "code name.py" to create python file
+# use "python name.py" to execute file
+
+name = input("What is your name? ")
+
+name = name.strip() # remove whitespaces from str
+
+print(f"Hello, {name}") # print is a function that takes arguments
